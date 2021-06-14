6881e0dfd825d3dcb73f0fa2a777003ee9ec39f7
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-14 13:31:17 +0800

    adding favicon and sematic ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("semantic-ui-sass")
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
