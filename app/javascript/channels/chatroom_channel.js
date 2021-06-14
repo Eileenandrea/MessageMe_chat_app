3a3426427c760af5394877fa927c7f4a4643a099
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-14 18:35:56 +0800

    complete chat app

---
+++ app/javascript/channels/chatroom_channel.js
@@ -0,0 +1,22 @@
+import consumer from "./consumer"
+
+const scroll_bottom = function () {
+  if ($('#messages').length > 0) {
+      $('#messages').scrollTop($('#messages')[0].scrollHeight)
+  }
+}
+consumer.subscriptions.create("ChatroomChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+  // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    $('#message-container').append(data.mod_message)
+    scroll_bottom();
+    // Called when there's incoming data on the websocket for this channel
+  }
+});
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
     belongs_to :user
     validates :body, presence: true, length: {maximum: 240}
+    scope :custom_display, -> { order(:created_at).last(500) }
 end
